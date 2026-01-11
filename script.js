018117de4c21fb0a2ddd52a746ecb995ef7fcbc5
jcjd0708 <davidjuancarlos1127@gmail.com>
2026-01-11 23:36:07 +0800

    added tax breakdown

--- script.js
+++ script.js
@@ -1,37 +1,103 @@
-const form = document.querySelector('#tax_form');
-const total_contribution = document.querySelector("#total_contribution");
-const sss = document.querySelector('#sss');
-const pagibig = document.querySelector('#pagibig');
-const philhealth = document.querySelector('#philhealth');
-const trainLaw = document.querySelector('#trainLaw');
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("tax_form");
+    const totalEmployeeContribution = document.getElementById("total_employee_contribution");
+    const sssEmployee = document.getElementById("sss_employee");
+    const pagibigEmployee = document.getElementById("pagibig_employee");
+    const philhealthEmployee = document.getElementById("philhealth_employee");
+    const withholdingTax = document.getElementById("withholding_tax");
+    const salary = document.getElementById("salary");
+    const frequency = document.getElementById("frequency");
+    const addDeduction = document.getElementById("addDeduction");
+    const otherDeductions = document.getElementById("other_deductions");
+    const netPay = document.getElementById("net_pay");
+    const taxBracket = document.getElementById("tax-bracket");
+    const baseTax = document.getElementById('base-tax');
+    const incomeExcess = document.getElementById('income-excess');
+    const taxRate = document.getElementById('tax-rate');
+    const excessTax = document.getElementById('excess-tax');
+    const totalWithholdingTax = document.getElementById('total-withholding-tax');
+    const taxableIncome = document.getElementById('taxable-income');
+    const trainLawTableContainer = document.getElementById('train-law-table-container');
+    
 
-let salary = document.querySelector("#salary");
-salary.addEventListener("input", updateTax);
-let frequency = document.querySelector('#frequency');
-frequency.addEventListener("change", updateTax);
+    salary.addEventListener("input", updateTax);
+    frequency.addEventListener("change", updateTax);
 
-let addDeductions = document.querySelector("#addDeduction");
-addDeductions.addEventListener("click", (e) => {
-    e.preventDefault();
+    addDeduction.addEventListener("click", (e) => {
 
-});
+        e.preventDefault();
 
+        const inputContainer = document.getElementById("input_container");
 
-function updateTax() {
+        const deductionDiv = document.createElement("div");
 
-    const formData = new FormData(form);
+        const input = document.createElement("input");
+        input.type = "number";
+        input.name = "deductions[]";
+        input.placeholder = "PHP 0.00";
+        input.addEventListener("input", updateTax);
+        
 
-    fetch('tax_calculator.php', {
-        method: 'POST',
-        body: formData
+        const removeBtn = document.createElement("button");
+        removeBtn.type = "button";
+        removeBtn.textContent = 'x';
+        removeBtn.addEventListener("click", () => {
+            removeButton(removeBtn);
+            updateTax();
+        })
+
+        deductionDiv.append(input, removeBtn);
+        inputContainer.appendChild(deductionDiv);
     })
-    .then(response => response.json())
-    .then(result => {
-        total_contribution.value = result.totalContributions !== '' ? "₱ " + parseFloat(result.totalContributions).toFixed(2) : '';
-        sss.value = result.sss !== '' ? "₱ " + parseFloat(result.sss).toFixed(2) : '';
-        pagibig.value = result.pagibig !== '' ? "₱ " + parseFloat(result.pagibig).toFixed(2) : '';
-        philhealth.value = result.philhealth !== '' ? "₱ " + parseFloat(result.philhealth).toFixed(2) : '';
-        trainLaw.value = result.trainTax !== '' ? "₱ " + parseFloat(result.trainTax).toFixed(2) : '';
-    })
-}
 
+    function removeButton(button) {
+        button.parentElement.remove();
+    }
+
+
+    function updateTax() {
+        const formData = new FormData(form);
+        console.log("Salary value:", formData.get('salary'));
+
+        fetch('tax_calculator.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(result => {
+            totalEmployeeContribution.value = result.totalEmployeeContribution !== '' ? "₱ " + parseFloat(result.totalEmployeeContribution).toFixed(2) : '';
+            sssEmployee.value = result.sssEmployee !== '' ? "₱ " + parseFloat(result.sssEmployee).toFixed(2) : '';
+            philhealthEmployee.value = result.philhealthEmployee !== '' ? "₱ " + parseFloat(result.philhealthEmployee).toFixed(2) : '';
+            pagibigEmployee.value = result.pagibigEmployee !== '' ? "₱ " + parseFloat(result.pagibigEmployee).toFixed(2) : '';
+            withholdingTax.value = result.withholdingTax !== '' ? "₱ " + parseFloat(result.withholdingTax).toFixed(2) : '';
+            otherDeductions.value = result.otherDeductions !== '' ? "₱ " + parseFloat(result.otherDeductions).toFixed(2) : '';
+            netPay.value = result.netPay !== '' ? "₱ " + parseFloat(result.netPay).toFixed(2) : '';
+            
+            totalWithholdingTax.textContent = '₱ ' + parseFloat(result.withholdingTax).toFixed(2);
+            taxableIncome.textContent = '₱ ' + parseFloat(result.taxableIncome).toFixed(2);
+
+            const salaryValue = parseFloat(formData.get('salary')) || 0;
+            const netPayValue = parseFloat(result.netPay) || 0;
+
+            if(salaryValue <= 0 || netPayValue <= 0) {
+                trainLawTableContainer.style.display = 'none';
+                if(netPayValue < 0) {
+                    netPay.style.color = 'red';
+                } else {
+                netPay.style.color = 'black';
+                }
+            } else {
+                trainLawTableContainer.style.display = 'block';
+                netPay.style.color = 'black';
+                displayTaxBreakdown(result.trainLawBreakdown);
+            }    
+        })
+    }
+
+    function displayTaxBreakdown(breakdownParam) {
+        taxBracket.textContent = "(" + breakdownParam.taxRate + ") " + breakdownParam.bracket;
+        baseTax.textContent = "₱ " + parseFloat(breakdownParam.baseTax).toFixed(2);
+        incomeExcess.textContent = "₱ " + parseFloat(breakdownParam.incomeExcess).toFixed(2);
+        excessTax.textContent = "₱ " + parseFloat(breakdownParam.excessTax).toFixed(2);
+    }
+})
--- tax_calculator.php
+++ tax_calculator.php
@@ -1,6 +1,6 @@
 <?php
 
-function sssContribution($salary, $rate = 0.05) {
+function sssEmployeeContribution($salary, $rate = 0.05) {
     if($salary < 0) {
         return 0;
     }
@@ -75,6 +75,81 @@ function sssContribution($salary, $rate = 0.05) {
     return 0;
 }
 
+function sssEmployerContribution($salary, $rate = 0.1) {
+    if($salary < 0) {
+        return 0;
+    }
+    $salaryRange = [
+        ['min' => 0, 'max' => 5249.99, 'msc' => 5000, 'mpf' => 0, 'ec' => 10],
+        ['min' => 5250, 'max' => 5749.99, 'msc' => 5500, 'mpf' => 0, 'ec' => 10],
+        ['min' => 5750, 'max' => 6249.99, 'msc' => 6000, 'mpf' => 0, 'ec' => 10],
+        ['min' => 6250, 'max' => 6749.99, 'msc' => 6500 , 'mpf' => 0, 'ec' => 10],
+        ['min' => 6750, 'max' => 7249.99, 'msc' => 7000 , 'mpf' => 0, 'ec' => 10],
+        ['min' => 7250, 'max' => 7749.99, 'msc' => 7500 , 'mpf' => 0, 'ec' => 10],
+        ['min' => 7750, 'max' => 8249.99, 'msc' => 8000 , 'mpf' => 0, 'ec' => 10],
+        ['min' => 8250, 'max' => 8749.99, 'msc' => 8500 , 'mpf' => 0, 'ec' => 10],
+        ['min' => 8750, 'max' => 9249.99, 'msc' => 9000 , 'mpf' => 0, 'ec' => 10],
+        ['min' => 9250, 'max' => 9749.99, 'msc' => 9500, 'mpf' => 0, 'ec' => 10],
+        ['min' => 9750, 'max' => 10249.99, 'msc' => 10000 , 'mpf' => 0, 'ec' => 10],
+        ['min' => 10250, 'max' => 10749.99, 'msc' => 10500 , 'mpf' => 0, 'ec' => 10],
+        ['min' => 10750, 'max' => 11249.99, 'msc' => 11000, 'mpf' => 0, 'ec' => 10],
+        ['min' => 11250, 'max' => 11749.99, 'msc' => 11500, 'mpf' => 0, 'ec' => 10],
+        ['min' => 11750, 'max' => 12249.99, 'msc' => 12000, 'mpf' => 0, 'ec' => 10],
+        ['min' => 12250, 'max' => 12749.99, 'msc' => 12500, 'mpf' => 0, 'ec' => 10],
+        ['min' => 12750, 'max' => 13249.99, 'msc' => 13000, 'mpf' => 0, 'ec' => 10],
+        ['min' => 13250, 'max' => 13749.99, 'msc' => 13500, 'mpf' => 0, 'ec' => 10],
+        ['min' => 13750, 'max' => 14249.99, 'msc' => 14000, 'mpf' => 0, 'ec' => 10],
+        ['min' => 14250, 'max' => 14749.99, 'msc' => 14500, 'mpf' => 0, 'ec' => 10],
+        ['min' => 14750, 'max' => 15249.99, 'msc' => 15000, 'mpf' => 0, 'ec' => 30],
+        ['min' => 15250, 'max' => 15749.99, 'msc' => 15500, 'mpf' => 0, 'ec' => 30],
+        ['min' => 15750, 'max' => 16249.99, 'msc' => 16000, 'mpf' => 0, 'ec' => 30],
+        ['min' => 16250, 'max' => 16749.99, 'msc' => 16500, 'mpf' => 0, 'ec' => 30],
+        ['min' => 16750, 'max' => 17249.99, 'msc' => 17000, 'mpf' => 0, 'ec' => 30],
+        ['min' => 17250, 'max' => 17749.99, 'msc' => 17500, 'mpf' => 0, 'ec' => 30],
+        ['min' => 17750, 'max' => 18249.99, 'msc' => 18000, 'mpf' => 0, 'ec' => 30],
+        ['min' => 18250, 'max' => 18749.99, 'msc' => 18500, 'mpf' => 0, 'ec' => 30],
+        ['min' => 18750, 'max' => 19249.99, 'msc' => 19000, 'mpf' => 0, 'ec' => 30],
+        ['min' => 19250, 'max' => 19749.99, 'msc' => 19500, 'mpf' => 0, 'ec' => 30],
+        ['min' => 19750, 'max' => 20249.99, 'msc' => 20000, 'mpf' => 0, 'ec' => 30],
+        ['min' => 20250, 'max' => 20749.99, 'msc' => 20000, 'mpf' => 50, 'ec' => 30],
+        ['min' => 20750, 'max' => 21249.99, 'msc' => 20000, 'mpf' => 100, 'ec' => 30],
+        ['min' => 21250, 'max' => 21749.99, 'msc' => 20000, 'mpf' => 150, 'ec' => 30],
+        ['min' => 21750, 'max' => 22249.99, 'msc' => 20000, 'mpf' => 200, 'ec' => 30],
+        ['min' => 22250, 'max' => 22749.99, 'msc' => 20000, 'mpf' => 250, 'ec' => 30],
+        ['min' => 22750, 'max' => 23249.99, 'msc' => 20000, 'mpf' => 300, 'ec' => 30],
+        ['min' => 23250, 'max' => 23749.99, 'msc' => 20000, 'mpf' => 350, 'ec' => 30],
+        ['min' => 23750, 'max' => 24249.99, 'msc' => 20000, 'mpf' => 400, 'ec' => 30],
+        ['min' => 24250, 'max' => 24749.99, 'msc' => 20000, 'mpf' => 450, 'ec' => 30],
+        ['min' => 24750, 'max' => 25249.99, 'msc' => 20000, 'mpf' => 500, 'ec' => 30],
+        ['min' => 25250, 'max' => 25749.99, 'msc' => 20000, 'mpf' => 550, 'ec' => 30],
+        ['min' => 25750, 'max' => 26249.99, 'msc' => 20000, 'mpf' => 600, 'ec' => 30],
+        ['min' => 26250, 'max' => 26749.99, 'msc' => 20000, 'mpf' => 650, 'ec' => 30],
+        ['min' => 26750, 'max' => 27249.99, 'msc' => 20000, 'mpf' => 700, 'ec' => 30],
+        ['min' => 27250, 'max' => 27749.99, 'msc' => 20000, 'mpf' => 750, 'ec' => 30],
+        ['min' => 27750, 'max' => 28249.99, 'msc' => 20000, 'mpf' => 800, 'ec' => 30],
+        ['min' => 28250, 'max' => 28749.99, 'msc' => 20000, 'mpf' => 850, 'ec' => 30],
+        ['min' => 28750, 'max' => 29249.99, 'msc' => 20000, 'mpf' => 900, 'ec' => 30],
+        ['min' => 29250, 'max' => 29749.99, 'msc' => 20000, 'mpf' => 950, 'ec' => 30],
+        ['min' => 29750, 'max' => 30249.99, 'msc' => 20000, 'mpf' => 1000, 'ec' => 30],
+        ['min' => 30250, 'max' => 30749.99, 'msc' => 20000, 'mpf' => 1050, 'ec' => 30],
+        ['min' => 30750, 'max' => 31249.99, 'msc' => 20000, 'mpf' => 1100, 'ec' => 30],
+        ['min' => 31250, 'max' => 31749.99, 'msc' => 20000, 'mpf' => 1150, 'ec' => 30],
+        ['min' => 31750, 'max' => 32249.99, 'msc' => 20000, 'mpf' => 1200, 'ec' => 30],
+        ['min' => 32250, 'max' => 32749.99, 'msc' => 20000, 'mpf' => 1250, 'ec' => 30],
+        ['min' => 32750, 'max' => 33249.99, 'msc' => 20000, 'mpf' => 1300, 'ec' => 30],
+        ['min' => 33250, 'max' => 33749.99, 'msc' => 20000, 'mpf' => 1350, 'ec' => 30],
+        ['min' => 33750, 'max' => 34249.99, 'msc' => 20000, 'mpf' => 1400, 'ec' => 30],
+        ['min' => 34250, 'max' => 34749.99, 'msc' => 20000, 'mpf' => 1450, 'ec' => 30],
+        ['min' => 34750, 'max' => INF, 'msc' => 20000, 'mpf' => 1500, 'ec' => 30]
+    ];
+    foreach($salaryRange as $row) {
+        if($salary >= $row['min'] && $salary <= $row['max']) {
+            return $row['msc'] * $rate + $row['mpf'] + $row['ec'];
+        }
+    }
+    return 0;
+}
+
 function philhealthContribution($salary, $rate = 0.05) {
     $min = 10000;
     $max = 100000;
@@ -88,13 +163,22 @@ function philhealthContribution($salary, $rate = 0.05) {
     }
 }
 
-function pagibigContributions($salary, $rate = 0.02) {
+function pagibigEmployeeContributions($salary, $rate = 0.02) {
     $min = 1500;
     $max = 10000;
     if($salary <= $min) {
         $rate = 0.01;
         return $salary * $rate;
     } else if ($salary > $max) {
+        return $max * $rate;
+    } else {
+        return $salary * $rate;
+    }
+}
+
+function pagibigEmployerContributions($salary, $rate = 0.02) {
+    $max = 10000;
+    if($salary > $max) {
         return $max * $rate;
     } else {
         return $salary * $rate;
@@ -118,7 +202,13 @@ function salaryConversion($salary, $frequency) {
 
 function calculateTrainTax($taxableIncome) {
     if($taxableIncome < 0) {
-        return 0;
+        return [
+            'withholdingTax' => 0,
+            'baseTax' => 0,
+            'taxRate' => 0 . '%',
+            'incomeExcess' => 0,
+            'bracket' => 'Below Minimum'
+        ];
     }
     
     $salaryRange = [
@@ -131,44 +221,95 @@ function calculateTrainTax($taxableIncome) {
     ];
     foreach($salaryRange as $row) {
         if($taxableIncome >= $row['min'] && $taxableIncome <= $row['max']) {
-            return $row['tax'] + (($taxableIncome - $row['min']) * $row['rate']);
+            $rate = $row['rate'];
+            $baseTax = $row['tax'];
+            $minTax = $row['min'];
+            $excess = $taxableIncome - $minTax;
+            $excessTax = $excess * $rate;
+            $withholdingTax = $excessTax + $baseTax;
+            $maxDisplay = $row['max'] == INF ? 'Above' : number_format($row['max']);
+            $bracketString = '₱' . number_format($row['min']) . ' - ' . '₱' . $maxDisplay;
+            return [
+                'withholdingTax' => $withholdingTax,
+                'baseTax' => $baseTax,
+                'taxRate' => ($rate * 100) . '%',
+                'incomeExcess' => $excess,
+                'excessTax' => $excessTax,
+                'bracket' => $bracketString
+            ];
         }
     }
-    return 0;
-} 
+    return [
+        'withholdingTax' => 0,
+        'baseTax' => 0,
+        'taxRate' => 0,
+        'incomeExcess' => 0,
+        'excessTax' => 0,
+        'bracket' => 'Unknown'
+    ];
+}
+
+function otherDeductions() {
+    $otherDeductions = 0;
+    if(isset($_POST['deductions']) && is_array($_POST['deductions'])) {
+        foreach($_POST['deductions'] as $deduction) {
+            $otherDeductions += floatval($deduction);
+        }
+    }
+    return $otherDeductions;
+}
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $salary = floatval($_POST['salary']);
     $frequency = $_POST['frequency'];
     
     $monthly = salaryConversion($salary, $frequency);
-    $sss = sssContribution($monthly);
-    $philhealth = philhealthContribution($monthly);
-    $pagibig = pagibigContributions($monthly);
-    $totalContributions = $sss + $philhealth + $pagibig;
-    $taxableIncome = $monthly - $totalContributions;
-    $trainTax = calculateTrainTax($taxableIncome);
+    $sssEmployee = sssEmployeeContribution($monthly);
+    $philhealthEmployee = philhealthContribution($monthly);
+    $pagibigEmployee = pagibigEmployeeContributions($monthly);
+    $otherDeductions = otherDeductions();
+    $totalEmployeeContribution = $sssEmployee + $philhealthEmployee + $pagibigEmployee;
+
+    $sssEmployer = sssEmployerContribution($monthly);
+    $philhealthEmployer = philhealthContribution($monthly);
+    $pagibigEmployer = pagibigEmployerContributions($monthly);
+    $totalEmployerContribution = $sssEmployer + $philhealthEmployer + $pagibigEmployer;
+
+    $totalSssContribution = $sssEmployee + $sssEmployer;
+    $totalPhilhealthContribution = $philhealthEmployee + $philhealthEmployer;
+    $totalPagibigContribution = $pagibigEmployee + $pagibigEmployer;
+
+    $taxableIncome = $monthly - ($totalEmployeeContribution + $otherDeductions);
+    $trainLawData = calculateTrainTax($taxableIncome);
+    $withholdingTax = $trainLawData['withholdingTax'];
+    $netPay = $monthly - ($totalEmployeeContribution + $withholdingTax + $otherDeductions);
+    
     
     if($salary <= 0) {
         echo json_encode([
             'monthly' => '',
-            'sss' => '',
-            'philhealth' => '',
-            'pagibig' => '',
-            'totalContributions' => '',
+            'sssEmployee' => '',
+            'philhealthEmployee' => '',
+            'pagibigEmployee' => '',
+            'totalEmployeeContribution' => '',
             'taxableIncome' => '',
-            'trainTax' => ''
+            'withholdingTax' => '',
+            'otherDeductions' => '',
+            'netPay' => ''
         ]);
         exit;
     }
 
     echo json_encode([
         'monthly' => $monthly,
-        'sss' => $sss,
-        'philhealth' => $philhealth,
-        'pagibig' => $pagibig,
-        'totalContributions' => $totalContributions,
+        'sssEmployee' => $sssEmployee,
+        'philhealthEmployee' => $philhealthEmployee,
+        'pagibigEmployee' => $pagibigEmployee,
+        'totalEmployeeContribution' => $totalEmployeeContribution,
         'taxableIncome' => $taxableIncome,
-        'trainTax' => $trainTax
+        'withholdingTax' => $withholdingTax,
+        'otherDeductions' => $otherDeductions,
+        'netPay' => $netPay,
+        'trainLawBreakdown' => $trainLawData
     ]);
 }
